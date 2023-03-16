0df0ac462e97a66b3a91c24881eafd7e427680d3
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2023-03-16 14:02:58 +0530

    ObjectPage extension sample code flexible programming model

---
+++ app/risks/webapp/ext/RisksObjectPage.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define(
+    [
+        'sap/fe/core/PageController',
+        'sap/ui/model/json/JSONModel',
+    ],
+    function (PageController, JSONModel,) {
+        'use strict';
+
+        return PageController.extend('ns.risks.ext.RisksObjectPage', {
+            onInit: function () {
+                PageController.prototype.onInit.apply(this);
+                let model = {
+                    editable: true
+                };
+                this.getView().setModel(new JSONModel(model), "viewModel");
+            },
+
+            saveDocument: function () {
+                var that = this;
+                this.editFlow.saveDocument(this.getView().getBindingContext()).then(function () {
+                    that.getView().getModel("viewModel").setProperty("/editable", false);
+                })
+            },
+
+            cancelDocument: function () {
+                var that = this;
+                this.editFlow.cancelDocument(this.getView().getBindingContext(), {
+                    control: this.byId("cancelButton")
+                }).then(function () {
+                    that.getView().getModel("viewModel").setProperty("/editable", false);
+
+                })
+            }
+        });
+    }
+);
